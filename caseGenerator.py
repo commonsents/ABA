346e90ca130868c7eddfed4869090e18ba9353df
Karston Kelly <kkelly@sandiego.edu>
2021-05-11 13:23:12 -0700

    Added missing ADU command to fuzzer

--- caseGenerator.py
+++ caseGenerator.py
@@ -1,7 +1,7 @@
 #Generates the random input to be piped into the program.
 import random
-
-def fuzzer(max_length=1000, char_start=32, char_range=32):
+import sys
+def fuzzer(max_length=100, char_start=32, char_range=32):
     """A string of up to `max_length` characters
        in the range [`char_start`, `char_start` + `char_range`]"""
     string_length = random.randrange(0, max_length + 1)
@@ -17,49 +17,53 @@ if __name__ == "__main__":
         inputs.append(fuzzer())
         x += 1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("IMD " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("CHP " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("DLU " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("EDR " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("ADR " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("DEU " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("LSU " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("DAL " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("DER " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
         inputs.append("RER " + fuzzer())
         x +=1
     x = 0
-    while x < 100:
+    while x < 1000:
+        inputs.append("ADU " + fuzzer())
+        x +=1
+    x = 0
+    while x < 1000:
         inputs.append("HLP " + fuzzer())
         x +=1
     for y in inputs:
-        print(y)
+        sys.stdout.write(y)
     print("EXT")
